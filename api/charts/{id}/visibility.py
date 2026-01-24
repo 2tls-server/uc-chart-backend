82788adb4165bb55428adf06ec2eded54dc5a56c
YumYummity <103061664+YumYummity@users.noreply.github.com>
2026-01-24 13:00:35 -0500

    fix: remove visibility webhook

--- api/charts/{id}/visibility.py
+++ api/charts/{id}/visibility.py
@@ -176,44 +176,6 @@ async def main(
                 )
                 wmsg.add_embed(wembed)
                 await wmsg.send()
-            if (
-                app.config["discord"]["published-webhook"]
-                if result.is_first_publish
-                else ""
-            ).strip() != "":
-                wmsg = WebhookMessage(
-                    app.config["discord"]["published-webhook"],
-                    app.config["discord"]["avatar-url"],
-                    app.config["discord"]["username"],
-                )
-                wembeds = [
-                    WebhookEmbed()
-                    .set_description(
-                        "## 創作譜面が公開されました / New Chart Published!"
-                    )
-                    .set_color("PURPLE")
-                ]
-                wembed = (
-                    WebhookEmbed()
-                    .set_title(sanitize_md(result.title))
-                    .set_description(
-                        f"- *{sanitize_md(result.artists)}*\n譜面作者 / Charted by: `{sanitize_md(result.author_full)}`\n\n今すぐプレイ！ / Play it now!\n{url_creator(app.config['server']['sonolus-server-url'], 'levels', app.config['server']['sonolus-server-chart-prefix'] + result.id, as_sonolus_open=True)}"
-                    )
-                    .set_timestamp(True)
-                    .set_thumbnail(
-                        url_creator(
-                            app.s3_asset_base_url,
-                            result.author,
-                            result.id,
-                            result.jacket_file_hash,
-                        )
-                    )
-                    .set_color("BLUE")
-                )
-                wembeds.append(wembed)
-                for embed in wembeds:
-                    wmsg.add_embed(embed)
-                await wmsg.send()
             if user.mod:
                 d["mod"] = True
             if user.sonolus_id == d["author"]:
