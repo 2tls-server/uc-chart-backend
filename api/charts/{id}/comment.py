76dcc80aebabee185cef1db13249e119b9233b1b
YumYummity <103061664+YumYummity@users.noreply.github.com>
2026-05-01 21:17:07 -0400

    feat: increase comment limit to 1500 characters

--- api/charts/{id}/comment.py
+++ api/charts/{id}/comment.py
@@ -31,10 +31,10 @@ async def main(
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid chart ID."
         )
-    if len(data.content) > 200:
+    if len(data.content) > 1500:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Comments cannot be larger than 200 characters.",
+            detail="Comments cannot be larger than 1500 characters.",
         )
     user = await session.user()
     query = comments.create_comment(
