4a86c5bf87ea96a9c0a1d43f9344bf14ad34ef4c
YumYummity <103061664+YumYummity@users.noreply.github.com>
2026-05-02 18:01:59 -0400

    fix: delete account script

--- scripts/delete_account.py
+++ scripts/delete_account.py
@@ -3,7 +3,7 @@ import yaml
 
 id = ""
 
-url = "http://127.0.0.1:39000/api/accounts/{id}/"
+url = f"http://127.0.0.1:39000/api/accounts/{id}/"
 
 with open("config.yml", "r") as file:
     config = yaml.safe_load(file)
